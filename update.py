529862a3b56bf4b22daa8c0a86a9edc25d930687
gadhagod <69025547+gadhagod@users.noreply.github.com>
2021-02-06 11:05:06 -0800

    Added functional gcalendar compatibility

--- update.py
+++ update.py
@@ -1,3 +1,4 @@
+import pretty_errors
 import datetime
 from json import loads
 import pickle
@@ -8,18 +9,21 @@ import schoolopy
 
 sc_conf = loads(open('schoology.json', 'r').read())
 sc = schoolopy.Schoology(schoolopy.Auth(sc_conf['key'], sc_conf['secret']))
+sc.limit = 10*10*10
 me = sc.get_me()
-assignments = sc.get_assignments(section_id=2692851162)
+assignments = sc.get_assignments(section_id=sc_conf['classes'][0])
 
 today=datetime.datetime.today()
 
 recentAssignments=[]
 for i in assignments:
-    assignmentTime = datetime.datetime.strptime(i.due, "%Y-%m-%d %H:%M:%S")
+    if i['due'] == '':
+        continue
+    assignmentTime = datetime.datetime.strptime(i['due'], '%Y-%m-%d %H:%M:%S')
     tDelta=(assignmentTime-today).days
-    if (0<=tDelta<=6):
+    if (0 <= tDelta <= 15):
         recentAssignments.append(i)
-        print("Assignment: " + str(i))
+
 
 me['tz_offset'] = str(me['tz_offset'])
 if len(me['tz_offset'].replace('-', '')) == 1:
@@ -28,32 +32,38 @@ if len(me['tz_offset'].replace('-', '')) == 1:
 me['tz_offset'] = me['tz_offset'] + ':00'
 
 ##########################
-
     
-exit()
+for assignment in recentAssignments:
+    due = assignment['due'].replace(' ', 'T') + me['tz_offset']
 
-due = assignment['due'].replace(' ', 'T') + '-' + me['tz_offset']
+    try:
+        creds = pickle.load(open('token.pickle', 'rb'))
+    except FileNotFoundError:
+        raise Exception('Not authorized. Run "python3 sc-calendar login --help".')
+    service = build('calendar', 'v3', credentials=creds)
 
-try:
-    creds = pickle.load(open('token.pickle', 'rb'))
-except FileNotFoundError:
-    raise Exception('Not authorized. Run "python3 sc-calendar login --help".')
+    events = service.events().list(calendarId='primary').execute()['items']
+    event_names = []
+    for event in events:
+        event_names.append(event['summary'])
 
-service = build('calendar', 'v3', credentials=creds)
-event = {
-    'summary': assignment['title'],
-    'description': assignment['description'] + '\n\nYou can view this assignment at ' + assignment['web_url'],
-    'start': {
-        'dateTime': due,
-        'timeZone': me['tz_name']
-    },
-    'end': {
-        'dateTime': due,
-        'timeZone': me['tz_name']
-    },
-    'reminders': {
-        'useDefault': True,
-    }
-}
+    print(event_names)
+    if assignment['title'] not in event_names:
+        print(assignment['title'])
+        event = {
+            'summary': assignment['title'],
+            'description': assignment['description'] + '\n\nYou can view this assignment at ' + assignment['web_url'],
+            'start': {
+                'dateTime': due,
+                'timeZone': me['tz_name']
+            },
+            'end': {
+                'dateTime': due,
+                'timeZone': me['tz_name']
+            },
+            'reminders': {
+                'useDefault': True,
+            }
+        }
 
-service.events().insert(calendarId='primary', body=event).execute()
+        service.events().insert(calendarId='primary', body=event).execute()
